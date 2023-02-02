a31ebbcd354d5bf8f474c2ba1629f9ba4896b66d
prasadrevanaki <100789044+PrasadIRevanaki@users.noreply.github.com>
2023-02-02 20:46:55 +0530

    [Add]: Parse URI to extract tenantId and add as header.

---
+++ lib/resty/parseUri.lua
@@ -0,0 +1,17 @@
+-- Function to parse URI and extract tenantId
+function parse_uri( uri )
+  tenantId = ""
+    local count = 0
+    for token in string.gmatch(uri, "^%s/") do
+      print(token)
+        count = count + 1
+        -- URI example: /tenants/{tenantId}/users/{userId}/payments/{paymentId}/
+        -- break after reading tenantId path param and return tenantId
+        if(count = 2)
+          tenantId = token
+          break
+    end
+  return tenantId
+end
+
+return parse_uri(ngx.arg[1])
